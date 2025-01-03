f43b7ccf5746b5a62c0b9e25f3183d1944b5e17c
virajawate <viraj.awate@gmail.com>
2025-01-03 20:50:49 +0530

    Edited:AddedCommentsandCreateGridLogic

--- src/Map.cc
+++ src/Map.cc
@@ -4,28 +4,36 @@
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/System/Vector2.hpp>
 
+/* Constructor
 
+
+ */
 Map::Map(float cellsize, int width, int height) : cellSize(cellsize), grid(height, std::vector(width, 0)){}
 
 void Map::draw(sf::RenderTarget &target){
+  // vector grid defines the dimensions for the win
   if (grid.empty()){return;}
 
+  // Text Variable Formate
+  // Create a backgroud with grid size [Row, Col]
   sf::RectangleShape background(sf::Vector2f(
     static_cast<float>(grid[0].size()) * cellSize,
     static_cast<float>(grid.size()) * cellSize));
-  background.setFillColor(sf::Color::White);
+
+  // Set backgroud color to [white]
+  background.setFillColor(sf::Color::Red);
   target.draw(background);
 
+  // Lets draw individual cells (Smaller than cell size)
   sf::RectangleShape cell(sf::Vector2f(cellSize * 0.95f, 0.95f * cellSize));
-  cell.setFillColor(sf::Color::Blue);
 
   for(size_t r=0; r<grid.size(); r++){
     for(size_t c=0; c<grid[r].size(); c++){
-      if(grid[r][c] == 1){
-        cell.setPosition(c*cellSize, r*cellSize);
-        target.draw(cell);
-      }
+
+      if(grid[r][c] == 0){cell.setFillColor(sf::Color::Black);}
+      else if(grid[r][c] == 1){cell.setFillColor(sf::Color::White);}
+      cell.setPosition(c*cellSize, r*cellSize);
+      target.draw(cell);
     }
   }
-  
 }
