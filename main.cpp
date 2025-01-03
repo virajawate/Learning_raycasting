d072dfa72bc56f0f4e81bbbea4786b9f17fbfd58
virajawate <viraj.awate@gmail.com>
2025-01-03 20:54:09 +0530

    Edited:ChangedWinResolutionandAddedComments

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@
 #include "Map.h"
 
 int main() {
-  sf::RenderWindow win(sf::VideoMode(1200,675), "Raycasting");
-  Map map(32.0f, 50, 50);
+  float Num_cells = 25.0;
+  int Length = 25;
+  int Width = 30;
+
+  sf::RenderWindow win(sf::VideoMode(1366,768), "Raycasting");
+
+  Map map(Num_cells, Length, Width); // Backgroud is [num_cellsxLength,num_cellsxWidth]
 
   while(win.isOpen()){
     sf::Event buffer_event;
