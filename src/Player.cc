279d1b15c9c5545811b5ad4368b229f9ffe4d003
viraj.awate <viraj.awate@gmail.com>
2026-06-28 23:44:16 +0530

    Editted:CorrectedPlayerPoseGetterFn

--- src/Player.cc
+++ src/Player.cc
@@ -1,5 +1,5 @@
 #include "Player.h"
-#include <iostream>
+
 #include <math.h>
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
@@ -17,8 +17,8 @@ std::vector<float> Player::get_player_pose(){
     */
 
     std::vector<float> player_pose;
-    player_pose[0] = position.x;
-    player_pose[1] = position.y;
+    player_pose.push_back(position.x);
+    player_pose.push_back(position.y);
     player_pose.push_back(angle);
     return player_pose;
 }
