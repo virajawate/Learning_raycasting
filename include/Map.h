7e4335f8eb663a28af8e61f8014ee8aec0eef55b
virajawate <viraj.awate@gmail.com>
2026-07-05 19:19:29 +0530

    Added:ColoredMapConstructor

--- include/Map.h
+++ include/Map.h
@@ -1,6 +1,7 @@
 #ifndef _MAP_H
 #define _MAP_H
 
+#include <string>
 #include <vector>
 #include <SFML/Graphics/RenderTarget.hpp>
 
@@ -10,10 +11,12 @@ public:
   //Constructor with grid and cell dimensions
   Map(float cellsize, int width, int height);
   Map(float cellsize, std::vector<std::vector<int>> Grid);
+  Map(float cellsize, const std::string &filename);
 
   // Draw cells
   void draw(sf::RenderTarget& target);
   const std::vector<std::vector<int>> getGrid() const;
+  const std::vector<std::vector<sf::Color>> getGrid_color() const;
   float getCellsize() const;
 private:
   // Mapping Grid ROW<COL>
