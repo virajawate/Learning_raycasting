852e16117e1d962234c884e92a8a67c2a041353d
Viraj Awate <44661049+virajawate@users.noreply.github.com>
2026-09-27 00:13:26 +0530

    Editted:ChangedMapPath

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ int main() {
   // Map Color_map(Cell_size, "map/map_1.png");
   // Map Color_map{Cell_size};
   Map Color_map;
-  if(std::filesystem::exists("latest.map")){
+  if(std::filesystem::exists("map/latest.map")){
     std::cout<<"Loaded Latest Map\n";
     Color_map.load("map/latest.map");
   } else {
@@ -100,11 +100,11 @@ int main() {
       render.cast3DNewRayGUI_new(win, player, Color_map);
     }else if(state == State::Editor){
       Color_map.drawColorGridTexture_new(win, Cell_size);
-      editor.run(win, Color_map);
+      editor.new_run(win, Color_map);
       player.draw(win);
     } else if(state == State::Original){
-      Color_map.drawColorGrid(win);
-      editor.run(win, Color_map);
+      Color_map.drawColorGrid(win, Cell_size);
+      editor.new_run(win, Color_map);
       player.draw(win);
     }
     ImGui::SFML::Render(win);
